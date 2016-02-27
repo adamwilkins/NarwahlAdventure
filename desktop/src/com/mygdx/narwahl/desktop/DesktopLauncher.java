fd54edfd3baff04ff5f85e6f2717fe953ae98f4a
Adam Wilkins <adamlukewilkins@gmail.com>
2016-02-26 20:55:59 -0500

    setup

---
+++ desktop/src/com/mygdx/narwahl/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.mygdx.narwahl.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mygdx.narwahl.NarwahlMain;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new NarwahlMain(), config);
+	}
+}
